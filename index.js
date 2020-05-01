2af9ddb4cca73156fa090097e7132b3126062b5c
Earth <earth_wendell_lopez@dlsu.edu.ph>
2020-05-02 04:23:55 +0800

    try

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const path = require('path');
 const db = require('./models/db.js');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.engine( 'hbs', exphbs({
   extname: 'hbs', 
