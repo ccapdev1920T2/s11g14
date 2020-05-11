a56e4a0150bc835da2db8f1eec5cd3eef1ab0d21
Earth <earth_wendell_lopez@dlsu.edu.ph>
2020-05-11 13:06:10 +0800

    update for db

--- add_data.js
+++ add_data.js
@@ -3,7 +3,6 @@ const db = require('./models/db.js');
 
 const User = require('./models/UserModel.js');
 const Item = require('./models/ItemModel.js');
-const Request = require('./models/RequestModel.js');
 const Review = require('./models/ReviewModel.js');
 
 db.connect();
--- delete_data.js
+++ delete_data.js
@@ -2,12 +2,10 @@ const db = require('./models/db.js');
 
 const User = require('./models/UserModel.js');
 const Item = require('./models/ItemModel.js');
-const Request = require('./models/RequestModel.js');
 const Review = require('./models/ReviewModel.js');
 
 db.connect();
 
 db.deleteMany(User);
 db.deleteMany(Item);
-db.deleteMany(Request);
 db.deleteMany(Review);
